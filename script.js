62d0e871e97d140c52a3bd5d54d4c44f75d49fa9
josicoder1 <jsphtadesse@gmail.com>
2025-08-12 09:02:35 -0700

    on the way

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuOpenButton = document.querySelector("#menu-open-button");
+menuOpenButton.addEventListener('click',()=>{
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+const menuCloseButton = document.querySelector("#menu-close-button");
+menuCloseButton.addEventListener('click',()=> menuOpenButton.click());
