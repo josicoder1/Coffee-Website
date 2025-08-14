c330447d0634778e89457b5f22b750724df6a478
josicoder1 <jsphtadesse@gmail.com>
2025-08-14 01:20:30 -0700

    Front-end website for a Coffee & Burger Café showcasing menu, gallery, and contact info.
    A modern and responsive front-end website for a Coffee & Burger Café. The site showcases the café’s menu, gallery, and contact information, providing a visually appealing and user-friendly experience for visitors. Built using HTML, CSS, and JavaScript, it’s easy to customize for any café or restaurant.

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
 menuOpenButton.addEventListener('click',()=>{
     document.body.classList.toggle("show-mobile-menu");
 });
 
 const menuCloseButton = document.querySelector("#menu-close-button");
+
 menuCloseButton.addEventListener('click',()=> menuOpenButton.click());
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+});
+
+// initialize swiper
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+//   responsive breakpoints
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    }
+  }
+});
